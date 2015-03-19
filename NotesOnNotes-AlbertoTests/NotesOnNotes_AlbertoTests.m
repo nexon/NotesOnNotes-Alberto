13fa6064a40f0854fa9804cf3095aeef1858033c
Alberto Lagos <alberto.lagos@gmail.com>
2015-03-18 21:39:32 -0300

    Initial Commit

---
+++ NotesOnNotes-AlbertoTests/NotesOnNotes_AlbertoTests.m
@@ -0,0 +1,40 @@
+//
+//  NotesOnNotes_AlbertoTests.m
+//  NotesOnNotes-AlbertoTests
+//
+//  Created by Alberto Lagos on 18-03-15.
+//  Copyright (c) 2015 Alberto Lagos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface NotesOnNotes_AlbertoTests : XCTestCase
+
+@end
+
+@implementation NotesOnNotes_AlbertoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
